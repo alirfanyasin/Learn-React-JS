5585c28669d2f5a125cea202370209a8c669ca2a
alirfanyasin <risayunita21@gmail.com>
2023-08-20 18:48:05 +0700

    feat: Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
-const Button = ({ variant, type, children }) => {
+import React from "react";
+
+// Menggunakan Class Component (Statefull Component)
+class ButtonCls extends React.Component {
+  render() {
+    return (
+      <button
+        type="Submit"
+        className={`h-10 px-6 font-semibold rounded-lg bg-red-700 text-white`}
+      >
+        This is Button
+      </button>
+    );
+  }
+}
+
+// Menggunakan Functional Component (Stateless Component)
+const Button = ({
+  variant = "bg-black",
+  type = "button",
+  children = "Button",
+}) => {
   return (
     <button
       type={type}
@@ -11,10 +32,13 @@ const Button = ({ variant, type, children }) => {
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen min-">
-      <Button type="Submit" variant="bg-red-700">
+    <div className="flex items-center justify-center h-screen min-100">
+      <ButtonCls></ButtonCls>
+      <Button type="Submit" variant="bg-slate-700">
         Test
       </Button>
+      <Button type="Submit">Test</Button>
+      <Button type="Submit"></Button>
     </div>
   );
 }
