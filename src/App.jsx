77d0410f478b2de0daf8777debb739d5556495f1
alirfanyasin <risayunita21@gmail.com>
2023-08-20 18:26:10 +0700

    setup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+const Button = ({ variant, type, children }) => {
+  return (
+    <button
+      type={type}
+      className={`h-10 px-6 font-semibold rounded-lg ${variant} text-white`}
+    >
+      {children}
+    </button>
+  );
+};
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-200">
-      Hello world!
-    </h1>
-  )
+    <div className="flex items-center justify-center h-screen min-">
+      <Button type="Submit" variant="bg-red-700">
+        Test
+      </Button>
+    </div>
+  );
 }
